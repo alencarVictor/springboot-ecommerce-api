0af8280fa5a330762ca9e1da8e226af337fa3e71
Victor Alencar <alencarfelkipe@gmail.com>
2026-04-25 17:18:34 -0300

    initial project structure

---
+++ src/test/java/com/victor/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
