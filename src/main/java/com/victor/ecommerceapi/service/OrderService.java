b9020464455904a7b996ec9283ca6b82b3588520
Victor Alencar <alencarfelkipe@gmail.com>
2026-04-25 18:02:49 -0300

    feat: adiciona geração de ID e configuração do OrderService

--- src/main/java/com/victor/ecommerceapi/service/OrderService.java
+++ src/main/java/com/victor/ecommerceapi/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.victor.ecommerceapi.service;
 
 import com.victor.ecommerceapi.model.Order;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class OrderService {
     //Atributo
     private List<Order> orders = new ArrayList<>();
+    private Long currentId = 1L;
 
     //Construtor vazio
     public OrderService() {
@@ -14,6 +18,7 @@ public class OrderService {
 
     //Metodos da classe
     public Order createOrder(Order order){
+        order.setId(currentId++);
         order.setTotal(order.calculateTotal());
         orders.add(order);
         return order;
