119c28a3c182ba2742356739a80d79a3e4e17f39
Victor Alencar <alencarfelkipe@gmail.com>
2026-04-28 22:53:18 -0300

    feat: integra API com AWS DynamoDB e configura persistência inicial

--- src/main/java/com/victor/ecommerceapi/service/OrderService.java
+++ src/main/java/com/victor/ecommerceapi/service/OrderService.java
@@ -6,31 +6,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderService {
     //Atributo
+    private final DynamoDbClient dynamoDbClient;
+
     private List<Order> orders = new ArrayList<>();
     private Long currentId = 1L;
 
-    //Construtor vazio
-    public OrderService() {
+    //Construtor
+    public OrderService(DynamoDbClient dynamoDbClient) {
+        this.dynamoDbClient = dynamoDbClient;
     }
+
 
     //Metodos da classe
 
     //POST cria order
-    public Order createOrder(Order order){
-        order.setId(currentId++);
+    public Order createOrder(Order order) {
 
+        order.setId(currentId++);
         order.setStatus(OrderStatus.CREATED);
         order.setCreatedAt(LocalDateTime.now());
         order.setTotal(order.calculateTotal());
+
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName("orders")
+                .item(
+                        Map.of(
+                                "id", AttributeValue.builder().s(order.getId().toString()).build(),
+                                "status", AttributeValue.builder().s(order.getStatus().name()).build(),
+                                "createdAt", AttributeValue.builder().s(order.getCreatedAt().toString()).build(),
+                                "total", AttributeValue.builder().n(String.valueOf(order.getTotal())).build()
+                        )
+                )
+                .build();
+
+        dynamoDbClient.putItem(request);
+
         orders.add(order);
+
         return order;
     }
     //GET  lista tudo
